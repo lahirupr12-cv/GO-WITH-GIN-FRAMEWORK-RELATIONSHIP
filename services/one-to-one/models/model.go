c2624fb016925b552ee9c937127d08663abf3065
lahirupr12-cv <lahirub@conceptvines.com>
2022-12-11 09:16:27 +0530

    completed one to one relationship

--- services/one-to-one/models/model.go
+++ services/one-to-one/models/model.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	Name      string
 	CompanyID int
-	Company   Company
+	Company   Company `gorm:"constraint:onUpdate:CASCADE,OneDelete:SET NULL"`
 }
 
 type Company struct {
